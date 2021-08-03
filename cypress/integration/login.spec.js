9838a853a22e2934e83ed465e384c489b60daaef
sonalijot <sonali.jot@gmail.com>
2021-08-03 13:52:38 +0100

    refactor: clean up

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,15 +3,15 @@
 describe('Test login endpoint /login', () => {
 
   it('Verify if the user can log in with valid credentials', () => {
-
+        // return 200
   })
 
   it('Verify if the user cannot login if credentials are invalid', () => {
-
+        // return 401 ( unauthorised )
   })
 
   it('Verify if the user cannot login if credentials are empty', () => {
-
+      // return 401 ( unauthorised )
   })
 
 })
